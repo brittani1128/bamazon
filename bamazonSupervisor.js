e935a2cbfecd6bf779571d87c1b1e082740cf11c
Brittani <brittani1128@gmail.com>
2018-08-27 22:55:09 -0500

    removed supervisor

--- bamazonSupervisor.js
+++
@@ -1,9 +0,0 @@
-function getDepartments() {
-    connection.query("SELECT department_name FROM products",function(err,res){
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++){
-            var department = res[i].department_name;
-            departments.push(department);
-        }
-    });
-};
